7f3ca030b971debeee91a1b29c115cb061c817e5
Marcos Antonio de Souza <masfson@gmail.com>
2026-03-01 19:32:03 -0300

    Posicionamento dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,18 +2,30 @@
 
 int main() {
 
-    int tabuleiro[10][10];
-    //Delclaração da matriz para o tabuleiro do jogo
-    // 0 - água
+        int tabuleiro[10][10];
+        int navioHorizontal[3] = {3, 3, 3};
+        int navioVertical[3] = {3, 3, 3};
 
-    // incializando todas as posições do tabuleiro com 0 usando um loops
-
-
+    //Inicializando o tabuleiro com 0 (água)    
     for(int i = 0; i < 10; i++) {
         for(int j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
         }
-    }
+    
+        //Navio Horizontal
+        tabuleiro[2][1] = navioHorizontal[0];
+
+        tabuleiro[2][2] = navioHorizontal[1];
+
+        tabuleiro[2][3] = navioHorizontal[2];
+
+        //Navio Vertical
+        tabuleiro[4][5] = navioVertical[0];
+
+        tabuleiro[5][5] = navioVertical[1];
+
+        tabuleiro[6][5] = navioVertical[2];
+
 
     printf("Tabulerio 10 x 10 criado com sucesso!\n");
 
